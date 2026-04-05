b192bfe0cf3aad3ad638fce7abf9ec2eb5781281
Nguyễn Quốc Anh <162429561+Anhhuhi123@users.noreply.github.com>
2026-04-05 08:54:23 +0700

    Feat/branch 2 (#2)
    * branch_2_1
    
    * branch_2_2
    
    ?????

--- be/app/routers/auth.py
+++ be/app/routers/auth.py
@@ -5,23 +5,6 @@ from fastapi import HTTPException, status
 db = get_database()
 router = APIRouter()
 
-@router.post("/auth/register",status_code=201)
-async def register(user: User):
-    username = user.username
-    password = user.password    
-    # Simulate a database check
-    existing_user = db.users.find_one({"username": username})
-    if existing_user:
-         raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Username already exists"
-        )
-    else:
-        db.users.insert_one({"username": username, "password": password})
-        return {"message": "Registration successful"}
-      
-
-
 
 @router.post("/auth/login", status_code=200)
 async def login(user: User):
@@ -35,4 +18,16 @@ async def login(user: User):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid credentials"
+        )
+
+@router.post("/auth/register", status_code=201)
+async def register(user: User):
+    username = user.username
+    password = user.password
+
+    existing_user = db.users.find_one({"username": username})
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already exists"
         )
